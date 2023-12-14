08a894ef002e807178171cb1e5506a1e58ecdaf6
jasminejones2002 <jazziejones02@yahoo.com>
2023-12-14 01:43:13 -0600

    first class lab complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice (0, 2);
+}
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const returnLastTwoDrivers = function(lastdrivers) {
+    return lastdrivers.slice (-2);
+}
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMultiplier) {
+    return function(x) {
+        return fareMultiplier * x;
+    }
+}
+createFareMultiplier();
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers)
+}
+selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers)
